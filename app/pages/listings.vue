f269bb56ea4af4872b70f01d21f1fcb9018a9ec2
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-06-23 22:27:35 -0400

    Revert "Merge branch 'heo'"
    This reverts commit 55c46844be8052ae408f314a2362b53f32d31077, reversing
    changes made to b1d7efc9fc78866fa9e479cc7910e2e314ee53e3.

--- app/pages/listings.vue
+++ app/pages/listings.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="home">
-    <navbar/>
-    <div class="content">
-      <search/>
-      <div v-if="query === null" class="flex flex-col justify-center items-center m-4">
-        <listing v-for="listing in listings" :key="listing" :listing="listing" />
-      </div>
-      <div v-else class="flex flex-col justify-center items-center m-4">
-        <listing v-for="listing in query" :key="listing" :listing="listing" />
+  <div id="home">
+    <navbar />
+    <div id="content">
+      <search />
+      <div class="flex flex-col justify-center items-center m-4">
+        <listing
+          v-for="listing in listings"
+          :key="listing"
+          :listing="listing"
+        />
       </div>
     </div>
   </div>
@@ -19,25 +20,37 @@ export default {
   data() {
     return {
       user: this.$store.state.user,
-      query: null
     }
   },
   async asyncData({ $axios }) {
     const listings = await $axios.$get('/queryListings')
-    return {listings}
-   },
-   watch: {
-    '$store.state.filters': async function() {
-      try {
-        const listings = await this.$axios.$get(`/filterListings/${this.$store.state.filters}`)
-        this.query = listings
-      } catch (error) {
-        console.log(error)
-      }
-    }
-   }
+    return { listings }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
+// @import "../assets/scss/variables.scss";
+// .content {
+//   display: flex;
+//   justify-content: center;
+//   align-items: start;
+// }
+
+// .filters {
+//   width: 30vw;
+//   min-height: 100vh;
+//   margin: 2rem 1rem;
+//   // background-color: skyblue;
+// }
+
+// .listings {
+//   width: 57vw;
+//   margin: 1rem;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: start;
+//   align-items: center;
+//   // background-color: lightcoral;
+// }
 </style>
