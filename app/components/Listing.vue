1d90e3e867b1732d44a36bb91fea553dc36d0982
jasonchen3805 <90630687+jchn3805@users.noreply.github.com>
2022-06-25 14:40:23 -0400

    x

--- app/components/Listing.vue
+++ app/components/Listing.vue
@@ -4,20 +4,21 @@
     class="w-full h-auto overflow-visible flex justify-center item-center rounded-lg shadow-md m-2"
   >
     <div id="content" class="w-full h-full flex justify-center items-start p-4">
-      <nuxt-link :to="`/listings/${listing._id}`"
+      <nuxt-link
+        :to="`/listings/${listing._id}`"
         id="info"
-        class="w-4/5 h-full flex justify-start items-start space-x-4" 
+        class="w-4/5 h-full flex justify-start items-start space-x-4"
       >
         <div id="pic" class="w-16 h-16">
           <img class="object-cover" :src="listing.org.avatar" alt="" />
         </div>
         <div id="container" class="flex flex-col justify-center items-start">
           <h4 class="text-md">{{ listing.org.affiliation }}</h4>
-          <h3 class="text-xl mb-2">{{ listing.position }}</h3>
+          <h3 class="text-xl mb-2 font-medium">{{ listing.position }}</h3>
           <div
             class="flex flex-col justify-center items-start space-y-2 sm:flex-row sm:space-x-2 sm:space-y-0"
           >
-            <Listing-type class="my-auto" :listing="listing" />
+            <listing-type class="my-auto" :listing="listing" />
             <location class="my-auto" :listing="listing" />
           </div>
           <p class="text-gray-500 text-sm mt-4" id="date">Posted {{ date }}</p>
