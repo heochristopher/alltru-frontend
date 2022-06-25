365912ddc367a875f9e86ae68a3a7df931f02eba
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-06-25 11:16:42 -0400

    .

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav" class="fixed top-0 left-0 w-full h-20 bg-white shadow-md z-50">
     <div id="content" class="w-full h-full flex justify-between items-center">
-      <h1 class="text-3xl ml-10 font-medium">Alltru</h1>
+      <h1 class="text-3xl ml-10 font-medium truncate"><NuxtLink to="/">Alltru</NuxtLink></h1>
       <div id="links" class="flex justify-center items-center mr-10 space-x-8">
         <NuxtLink to="/register" class="text-md">For Students</NuxtLink>
         <NuxtLink to="/register" class="text-md">For Organizations</NuxtLink>
