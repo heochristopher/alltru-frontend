15826df62a11dc60b984ab991684b6721d3ad9e9
jasonchen3805 <90630687+jchn3805@users.noreply.github.com>
2022-06-28 19:31:29 -0400

    edit pfp

--- app/components/FormInput.vue
+++ app/components/FormInput.vue
@@ -1,7 +1,7 @@
 <template>
   <input
     @input="updateValue($event.target.value)"
-    class="block w-full h-12 text-sm p-3 bg-zinc-100 rounded-md border-zinc-300 border-solid border"
+    class="block w-full h-12 p-3 bg-zinc-100 rounded-md border-zinc-300 border-solid border text-sm"
     :value="value"
   />
 </template>
