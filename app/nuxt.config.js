c454f0328557af2309b57a4c90d62e553e48df3e
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-06-29 00:10:37 -0400

    fix font

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -18,13 +18,16 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap'}
+    ],
   },
 
   devServerHandlers: [],
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~assets/css/main.css'],
+  css: ['~/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -63,14 +66,11 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    loaders: {
-      sass: {
-        implementation: require('sass'),
-      },
-      scss: {
-        implementation: require('sass'),
-      },
-    },
-  },
+  // build: {
+  //   postcss: {
+  //     plugins: {
+  //       tailwindcss: {}
+  //     }
+  //   }
+  // },
 }
--- app/pages/listings/index.vue
+++ app/pages/listings/index.vue
@@ -72,7 +72,7 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 #filters {
   -webkit-appearance: none;
   -ms-overflow-style: none;
