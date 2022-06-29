5f878bf89676240c98184d1998a87a827886eb8c
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-06-29 01:30:27 -0400

    .

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: true,
@@ -54,7 +56,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.NODE_ENV === 'development' ? "http://localhost:3000" : "https://api.alltru.app",
     credentials: true
   },
   
