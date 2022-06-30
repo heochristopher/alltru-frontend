a55ba30e71fb8fb08dcc09b6985d5eda17796c01
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-06-30 07:39:01 -0400

    Update nuxt.config.js

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -56,7 +56,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.NODE_ENV === 'development' ? "http://localhost:3000" : "https://api.alltru.app",
+    baseURL: process.env.NODE_ENV === 'production' ? "https://api.alltru.app" : "http://localhost:3000",
     credentials: true
   },
   
