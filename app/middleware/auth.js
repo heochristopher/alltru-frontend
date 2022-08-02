02c06f0504aa35cfc99c7e15c956a3224bf26196
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-08-02 16:28:51 -0400

    .

--- app/middleware/auth.js
+++
@@ -1,6 +0,0 @@
-export default function ({ store, redirect }) {
-  if (!store.state.user) {
-    redirect('/login')
-    return store.dispatch('GET_ALERT', { status: 400, data: 'You must be logged in to access that page' })
-  }
-}
